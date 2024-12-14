a38580f22ae951342ca4eebde0b17b6b38412d84
Nikolai Iasnov <1215572+efpies@users.noreply.github.com>
2024-12-14 05:49:22 +0100

    [PHT] Add methods to retrieve auth tokens

--- src/services.go
+++ src/services.go
@@ -3,16 +3,19 @@ package main
 import (
 	"pht/comments-processor/aws"
 	"pht/comments-processor/handlers/lambda"
+	"pht/comments-processor/model"
+	"pht/comments-processor/pht"
 	"pht/comments-processor/services"
 )
 
 type appServices struct {
 	infraServices      services.InfraLocator
+	phtServices        *pht.Locator
 	lambdaEventHandler lambda.EventHandler
 }
 
 func newAppServices() (*appServices, error) {
-	s, err := aws.NewLocator()
+	s, err := aws.NewLocator("/pht-comments-processor")
 	if err != nil {
 		return nil, err
 	}
@@ -27,7 +30,26 @@ func (s *appServices) init() error {
 		return err
 	}
 
-	s.lambdaEventHandler = services.NewLambdaEventHandler()
+	phtServices, err := pht.ProvideLocator(s.infraServices.ParamsProvider())
+	if err != nil {
+		return err
+	}
+
+	if err = phtServices.Init(); err != nil {
+		return err
+	}
+
+	phtRouter, err := pht.ProvideRouter(phtServices)
+	if err != nil {
+		return err
+	}
+
+	leh := services.NewLambdaEventHandler()
+	if err = leh.RegisterPlatformHandler(model.PlatformEnum.Pht, phtRouter); err != nil {
+		return err
+	}
+
+	s.lambdaEventHandler = leh
 
 	return nil
 }
